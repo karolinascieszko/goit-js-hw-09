b6fd391c85bff8ae14a66acd9453de6c81a0b110
Karolina Ścieszko <karolinascieszko@gmail.com>
2021-09-13 18:30:08 +0200

    Update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+import { qs, qsa } from './utils';
 
+const startBtn = qs('button[data-start]');
+const stopBtn = qs('button[data-stop]');
+const body = qs('body');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopBtn.disabled = true;
+
+const startChangeColor = () => {
+  timerId = setInterval(() => {
+    const backgroundColor = getRandomHexColor();
+    body.style.backgroundColor = backgroundColor;
+    console.log(backgroundColor);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, 1000);
+};
+
+const stopChangeColor = () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+};
+
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangeColor);
